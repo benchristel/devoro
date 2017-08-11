59f3b4b40adf6a52cdf7c9d0b8373fda9631bb47
Ben Christel <bchristel@pivotal.io>
2017-08-10 22:36:11 -0700

    Prioritize domains we've never seen before

--- crawler.rb
+++ crawler.rb
@@ -11,7 +11,7 @@ class Crawler
     count = redis.scard('urls')
     puts "There are #{count} urls to crawl..."
 
-    while url = redis.spop('urls')
+    while url = next_url
       sleep 1
       puts url
 
@@ -21,15 +21,27 @@ class Crawler
         .each   { |link|
           enqueue link
         }
-        .map  { |link| URI(link).host }
-        .uniq
-        .each { |domain| puts "        Found domain: #{domain}" }
+        .uniq { |link| URI(link).host }
+        .each { |link|
+          domain = URI(link).host
+          if domain
+            if not redis.sismember('domains', domain)
+              redis.sadd('domains', domain)
+              redis.sadd('freshurls', link)
+            end
+            puts "        Found domain: #{domain}"
+          end
+        }
 
       redis.sadd('crawled', url)
     end
   end
 
   private
+
+  def next_url
+    redis.spop('freshurls') || redis.spop('urls')
+  end
 
   def enqueue(url)
     redis.sadd 'urls', url
@@ -96,14 +108,20 @@ end
 RULES = [
   BanApexDomain.new('bbc'),
   BanApexDomain.new('facebook'),
+  BanApexDomain.new('flickr'),
   BanApexDomain.new('goo.gl'),
   BanApexDomain.new('google'),
   BanApexDomain.new('instagram'),
+  BanApexDomain.new('linkedin'),
   BanApexDomain.new('microsoft'),
+  BanApexDomain.new('pcmag'),
   BanApexDomain.new('pinterest'),
   BanApexDomain.new('snapchat'),
   BanApexDomain.new('soundcloud'),
+  BanApexDomain.new('t.co'),
+  BanApexDomain.new('tumblr'),
   BanApexDomain.new('twitter'),
+  BanApexDomain.new('yahoo'),
   BanApexDomain.new('youtube'),
   BanPathComponent.new(/^user/),
   BanPathComponent.new(/user$/),
